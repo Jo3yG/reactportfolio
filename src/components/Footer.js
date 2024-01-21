4db5bf637a03fb098b47f70827a732a1f6b9fed4
Jo3yG <nonuser31@yahoo.com>
2024-01-20 18:16:05 -0600

    added Foorter

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>My Full Stack Development Portfolio</strong> - a React site by{" "}
+          <a href="https://github.com/Jo3yG" target="_blank" rel="noreferrer">
+            Joey Gaytan
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
